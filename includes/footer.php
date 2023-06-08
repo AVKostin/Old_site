6944e558dad29da987bd7375eebe855b78cb8597
Alexander Kostin <asurv2@gmail.com>
2023-06-09 01:37:49 +0300

    update code for footer section

--- includes/footer.php
+++ includes/footer.php
@@ -78,7 +78,18 @@
                 </li>
             </ul>
         </div>
-
+            <!-- <menu class="footer__menu">
+                <li class="footer__menu_item">
+                    <a href="services.php" class="footer__menu_item-b link link--bold">Сімейний психотерапевт</a>
+                </li>
+                <li class="footer__menu_item">
+                    <a href="services.php" class="footer__menu_item-a footer__menu_item-b link link--bold">Дитячий психотерапевт</a>
+                </li>
+                <li class="footer__menu_item">
+                    <a href="services.php" class="footer__menu_item-a footer__menu_item-b link link--bold">Індивідуальні заняття</a>
+                </li>
+            </menu> -->
+            <div class="footer__col footer__col--type">
             <menu class="footer__menu">
                 <li class="footer__menu_item">
                     <a href="services.php" class="footer__menu_item-b link link--bold">Сімейний психотерапевт</a>
@@ -90,6 +101,7 @@
                     <a href="services.php" class="footer__menu_item-a footer__menu_item-b link link--bold">Індивідуальні заняття</a>
                 </li>
             </menu>
+            </div>
 
                 <a href="#" id="toTop" class="">
                         <span class="arrow_top">
@@ -97,18 +109,6 @@
                             width="80">
                         </span>
                         </a>
-                        <!-- <a href="#" id="toTop" class=""> <span class="arrow_top"><svg viewBox="0 0 384 512"> <path fill="currentColor" d="M54.63 246.6L192 109.3l137.4 137.4C335.6 252.9 343.8 256 352 256s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25l-160-160c-12.5-12.5-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25S42.13 259.1 54.63 246.6zM214.6 233.4c-12.5-12.5-32.75-12.5-45.25 0l-160 160c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0L192 301.3l137.4 137.4C335.6 444.9 343.8 448 352 448s16.38-3.125 22.62-9.375c12.5-12.5 12.5-32.75 0-45.25L214.6 233.4z"></path></svg></span>
-                        </a> -->
+            </div>
     </div>
-
-<!--
-        <a href="price.html" class="wallet__set">
-            <p class="wallet__text">
-                Оплата
-            </p>
-            <img src="assets/img/wallet.png" alt="" class="wallet__img">
-        </a> -->
-
-    </div>
-
 </footer>
