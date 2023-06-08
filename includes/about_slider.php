04582c8e37563bd02ea6977166da6725547d158c
Alexander Kostin <asurv2@gmail.com>
2023-06-09 01:36:32 +0300

    update code for about section

---
+++ includes/about_slider.php
@@ -0,0 +1,39 @@
+<section id="about" title="About Us Section">
+  <div class="container">
+    <div class="section__top">
+      <h2 class="main-title main-title--upper" data-aos="fade-left">Відгуки</h2>
+    </div>
+    <div class="sliderRev" data-aos="fade-down">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review1.jpg">
+              <img src="assets/img/review1.jpg" width="370" alt="#" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review2.jpg">
+              <img src="assets/img/review2.jpg" width="370" alt="#" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review3.jpg">
+              <img src="assets/img/review3.jpg" width="370" alt="#" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review4.jpg">
+              <img src="assets/img/review4.jpg" width="370" alt="#" loading="lazy">
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review5.jpg">
+              <img src="assets/img/review5.jpg" width="370" alt="#" loading="lazy">
+            </div>
+          </div>
+        </div>
+        <div class="sec-rev-pagination"></div>
+      </div>
+    </div>
+  </div>
+</section>
