5e22eefb265117534981408146a16681f045fed8
Alexander Kostin <asurv2@gmail.com>
2023-06-09 01:35:55 +0300

    update code for spec section

--- includes/our_spec.php
+++ includes/our_spec.php
@@ -4,109 +4,23 @@
                 Наші спеціалісти
             </h1>
             <div class="specialists__set">
-                <div class="specialists__wrapp">
-                    <img src="assets/img/specialists.png" alt="" class="specialists_img">
+
+                <div class="specialists__wrapp"  id="goroshenko">
+                    <img src="assets/img/expert1-1.webp" alt="Меланія Горошено" class="specialists_img">
                     <div class="specialists__kit">
                         <p class="specialists__text">
-                            Психолог
+                        Психолог, засновниця центру
                         </p>
                         <p class="specialists__text_mod">
-                            Катерина Іваненко
+                        Меланія Горошенко
                         </p>
                         <div class="education__set">
                             <img src="assets/img/education-1.png" alt="" class="education__img">
-                            <p class="education__text">Вища освіта</p>
+                            <p class="education__text">Диплом європейського зразка</p>
                         </div>
                         <div class="education__set">
                             <img src="assets/img/education-2.png" alt="" class="education__img">
-                            <p class="education__text">Підвищення кваліфікації</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-3.png" alt="" class="education__img">
-                            <p class="education__text">Підтримка 24/7</p>
-                        </div>
-                        <a href="" class="education__btn_1">
-                            Записатися
-                        </a>
-                        <a href="" class="education__btn_2">
-                            Детальніше
-                        </a>
-                    </div>
-                </div>
-                <div class="specialists__wrapp">
-                    <img src="assets/img/specialists.png" alt="" class="specialists_img">
-                    <div class="specialists__kit">
-                        <p class="specialists__text">
-                            Психолог
-                        </p>
-                        <p class="specialists__text_mod">
-                            Катерина Іваненко
-                        </p>
-                        <div class="education__set">
-                            <img src="assets/img/education-1.png" alt="" class="education__img">
-                            <p class="education__text">Вища освіта</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-2.png" alt="" class="education__img">
-                            <p class="education__text">Підвищення кваліфікації</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-3.png" alt="" class="education__img">
-                            <p class="education__text">Підтримка 24/7</p>
-                        </div>
-                        <a href="" class="education__btn_1">
-                            Записатися
-                        </a>
-                        <a href="" class="education__btn_2">
-                            Детальніше
-                        </a>
-                    </div>
-                </div>
-                <div class="specialists__wrapp">
-                    <img src="assets/img/specialists.png" alt="" class="specialists_img">
-                    <div class="specialists__kit">
-                        <p class="specialists__text">
-                            Психолог
-                        </p>
-                        <p class="specialists__text_mod">
-                            Катерина Іваненко
-                        </p>
-                        <div class="education__set">
-                            <img src="assets/img/education-1.png" alt="" class="education__img">
-                            <p class="education__text">Вища освіта</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-2.png" alt="" class="education__img">
-                            <p class="education__text">Підвищення кваліфікації</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-3.png" alt="" class="education__img">
-                            <p class="education__text">Підтримка 24/7</p>
-                        </div>
-                        <a href="" class="education__btn_1">
-                            Записатися
-                        </a>
-                        <a href="" class="education__btn_2">
-                            Детальніше
-                        </a>
-                    </div>
-                </div>
-                <div class="specialists__wrapp">
-                    <img src="assets/img/specialists.png" alt="" class="specialists_img">
-                    <div class="specialists__kit">
-                        <p class="specialists__text">
-                            Психолог
-                        </p>
-                        <p class="specialists__text_mod">
-                            Катерина Іваненко
-                        </p>
-                        <div class="education__set">
-                            <img src="assets/img/education-1.png" alt="" class="education__img">
-                            <p class="education__text">Вища освіта</p>
-                        </div>
-                        <div class="education__set">
-                            <img src="assets/img/education-2.png" alt="" class="education__img">
-                            <p class="education__text">Підвищення кваліфікації</p>
+                            <p class="education__text">Практика у провідних клініках України</p>
                         </div>
                         <div class="education__set">
                             <img src="assets/img/education-3.png" alt="" class="education__img">
@@ -121,7 +35,195 @@
                     </div>
                 </div>
 
+                <div class="specialists__wrapp" id="peters">
+                    <img src="assets/img/expert1-2.webp" alt="Валерія Петерс" class="specialists_img">
+                    <div class="specialists__kit">
+                        <p class="specialists__text">
+                            Психолог
+                        </p>
+                        <p class="specialists__text_mod">
+                        Валерія Петерс
+                        </p>
+                        <div class="education__set">
+                            <img src="assets/img/education-1.png" alt="" class="education__img">
+                            <p class="education__text">Понад вісім років навчання</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-2.png" alt="" class="education__img">
+                            <p class="education__text">Курси спільно із фахівцями з ООН</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-3.png" alt="" class="education__img">
+                            <p class="education__text">Підтримка 24/7</p>
+                        </div>
+                        <a href="" class="education__btn_1">
+                            Записатися
+                        </a>
+                        <a href="" class="education__btn_2">
+                            Детальніше
+                        </a>
+                    </div>
+                </div>
+
+                <div class="specialists__wrapp" id="moroz">
+                    <img src="assets/img/expert1-3.webp" alt="" class="specialists_img">
+                    <div class="specialists__kit">
+                        <p class="specialists__text">
+                            Психолог
+                        </p>
+                        <p class="specialists__text_mod">
+                            Софія Мороз
+                        </p>
+                        <div class="education__set">
+                            <img src="assets/img/education-1.png" alt="Софія Мороз" class="education__img">
+                            <p class="education__text">Ступінь магістра психології при МВС</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-2.png" alt="" class="education__img">
+                            <p class="education__text">Досвід роботи із кризовими випадками</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-3.png" alt="" class="education__img">
+                            <p class="education__text">Підтримка 24/7</p>
+                        </div>
+                        <a href="" class="education__btn_1">
+                            Записатися
+                        </a>
+                        <a href="" class="education__btn_2">
+                            Детальніше
+                        </a>
+                    </div>
+                </div>
+
+                <div class="specialists__wrapp" id="shinaikova">
+                    <img src="assets/img/expert1-7.webp" alt="Валерія Шайнікова" class="specialists_img">
+                    <div class="specialists__kit">
+                        <p class="specialists__text">
+                            Психолог
+                        </p>
+                        <p class="specialists__text_mod">
+                        Валерія Шайнікова
+                        </p>
+                        <div class="education__set">
+                            <img src="assets/img/education-1.png" alt="" class="education__img">
+                            <p class="education__text">Здобуто дві вищі освіти</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-2.png" alt="" class="education__img">
+                            <p class="education__text">Робота у психоневрологічній клініці</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-3.png" alt="" class="education__img">
+                            <p class="education__text">Підтримка 24/7</p>
+                        </div>
+                        <a href="" class="education__btn_1">
+                            Записатися
+                        </a>
+                        <a href="" class="education__btn_2">
+                            Детальніше
+                        </a>
+                    </div>
+                </div>
+
+                <div class="specialists__wrapp" id="david">
+                    <img src="assets/img/expert1-8.webp" alt="Уляна Давид" class="specialists_img">
+                    <div class="specialists__kit">
+                        <p class="specialists__text">
+                            Психолог
+                        </p>
+                        <p class="specialists__text_mod">
+                        Уляна Давид
+                        </p>
+                        <div class="education__set">
+                            <img src="assets/img/education-1.png" alt="" class="education__img">
+                            <p class="education__text">Практико-орієнтовне навчання у МВС</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-2.png" alt="" class="education__img">
+                            <p class="education__text">Активна практика з дітьми різного віку</p>
+                        </div>
+                        <div class="education__set">
+                            <img src="assets/img/education-3.png" alt="" class="education__img">
+                            <p class="education__text">Підтримка 24/7</p>
+                        </div>
+                        <a href="" class="education__btn_1">
+                            Записатися
+                        </a>
+                        <a href="" class="education__btn_2">
+                            Детальніше
+                        </a>
+                    </div>
+                </div>
 
             </div>
+            <div class="sec-rev-pagination"></div>
         </div>
     </section>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <style>
+  .specialists__set {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: scroll;
+    scroll-behavior: smooth;
+  }
+
+  .specialists__wrapp {
+    flex: 0 0 auto;
+    width: calc(100% / 3);
+    margin-right: 10px;
+    scroll-snap-align: center;
+  }
+
+  /* Змінюємо стилі для активного спеціаліста */
+  .specialists__wrapp.active {
+    transform: scale(1.1);
+    border: 2px solid #000;
+    /* Додаткові стилі для активного спеціаліста */
+  }
+  .swiper-pagination-bullet {
+  width: 8px;
+  height: 8px;
+  background-color: #000;
+  opacity: 0.5;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 5px;
+  cursor: pointer;
+}
+
+.swiper-pagination-bullet-active {
+  opacity: 1;
+}
+</style>
+
+<!-- <script>
+  const specialists = document.querySelectorAll('.specialists__wrapp');
+
+  // Ініціалізація слайдера
+  const specialistsSlider = new Swiper('.specialists__set', {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    scrollbar: {
+    el: '.sec-rev-pagination',
+    draggable: false,
+  },
+    breakpoints: {
+      1280: {
+        slidesPerView: 3,
+      },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Дозволяє клікати на пагінаційні точки
+    },
+    on: {
+      slideChange: function () {
+        const activeIndex = this.realIndex;
+        specialists.forEach((item, index) => {
+          item.classList.toggle('active', index === activeIndex);
+        });
+      },
+    },
+  });
+</script> -->
---
+++ includes/spec.php
@@ -0,0 +1,357 @@
+<section class="section sec-adapt__width specialist mb150">
+        <div class="container">
+          <div class="section__top">
+            <h2 class="main-title main-title--upper" data-aos="fade-left">Наші фахівці</h2>
+          </div>
+
+          <div class="section__content sliderSpec" data-aos="fade-down">
+
+            <ul class="specialist__list swiper-wrapper">
+              <li class="specialist__item swiper-slide" id="goroshenko">
+
+                <div class="specialist__item-img">
+                  <img src="assets/img/expert1-1.webp" alt="Меланія Горошенко" loading="lazy">
+                </div>
+
+                <div class="specialist__item-info">
+                  <div class="specialist__item-inner">
+                    <div class="grid-spec">
+                      <span class="specialist__item-spec">Психолог, засновниця центру</span>
+                      <h3 class="specialist__item-title">Меланія Горошенко</h3>
+                    </div>
+
+                    <ul class="specialist__item-list">
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img1.svg" alt="1">
+                        <span class="specialist__item-merit-txt">Диплом європейського зразка</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img2.svg" alt="2">
+                        <span class="specialist__item-merit-txt">Практика у провідних клініках України</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img3.svg" alt="3">
+                        <span class="specialist__item-merit-txt">Кваліфікований арт-терапевт</span>
+                      </li>
+                    </ul>
+
+                  </div>
+                  <button class="specialist__item-button">Записатися</button>
+                </div>
+
+              </li>
+              <li class="specialist__item swiper-slide" id="peters">
+                  <div class="specialist__item-img">
+                    <img src="assets/img/expert1-2.webp" alt="Валерія Петерс" loading="lazy">
+                  </div>
+
+                  <div class="specialist__item-info">
+                    <div class="specialist__item-inner">
+                      <div class="grid-spec">
+                        <span class="specialist__item-spec">психолог</span>
+                        <h3 class="specialist__item-title">Валерія Петерс</h3>
+                      </div>
+
+
+                      <ul class="specialist__item-list">
+                        <li class="specialist__item-merit">
+                          <img src="assets/img/specialist__item-img1.svg" alt="1">
+                          <span class="specialist__item-merit-txt">Понад вісім років навчання</span>
+                        </li>
+                        <li class="specialist__item-merit">
+                          <img src="assets/img/specialist__item-img2.svg" alt="2">
+                          <span class="specialist__item-merit-txt">Курси спільно із фахівцями з ООН</span>
+                        </li>
+                        <li class="specialist__item-merit">
+                          <img src="assets/img/specialist__item-img3.svg" alt="3">
+                          <span class="specialist__item-merit-txt">Авторка психологічних медитацій</span>
+                        </li>
+                      </ul>
+
+
+                    </div>
+                    <button class="specialist__item-button">Записатися</button>
+                  </div>
+
+              </li>
+              <li class="specialist__item swiper-slide" id="moroz">
+
+                <div class="specialist__item-img">
+                  <img src="assets/img/expert1-3.webp" alt="Софія Мороз" loading="lazy">
+                </div>
+
+                <div class="specialist__item-info">
+                  <div class="specialist__item-inner">
+                    <div class="grid-spec">
+                      <span class="specialist__item-spec">психолог</span>
+                      <h3 class="specialist__item-title">Софія Мороз</h3>
+                    </div>
+                    <ul class="specialist__item-list">
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img1.svg" alt="1">
+                        <span class="specialist__item-merit-txt">Ступінь магістра психології при МВС</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img2.svg" alt="2">
+                        <span class="specialist__item-merit-txt">Досвід роботи із кризовими випадками</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img3.svg" alt="3">
+                        <span class="specialist__item-merit-txt">Робота із сучасними техніками</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <button class="specialist__item-button">Записатися</button>
+                </div>
+
+
+              </li>
+              <li class="specialist__item swiper-slide" id="sherbiy">
+
+                <div class="specialist__item-img">
+                  <img src="assets/img/expert1-7.webp" alt="Вікторія Щербій" loading="lazy">
+                </div>
+
+                <div class="specialist__item-info">
+                  <div class="specialist__item-inner">
+                    <div class="grid-spec">
+                      <span class="specialist__item-spec">психолог</span>
+                      <h3 class="specialist__item-title">Валерія Шайнікова</h3>
+                    </div>
+                    <ul class="specialist__item-list">
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img1.svg" alt="1">
+                        <span class="specialist__item-merit-txt">Здобуто дві вищі освіти</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img2.svg" alt="2">
+                        <span class="specialist__item-merit-txt">Робота у психоневрологічній клініці</span>
+                      </li>
+                      <li class="specialist__item-merit">
+                        <img src="assets/img/specialist__item-img3.svg" alt="3">
+                        <span class="specialist__item-merit-txt">Використовує методи символдрами</span>
+                      </li>
+                    </ul>
+
+                  </div>
+                  <button class="specialist__item-button">Записатися</button>
+                </div>
+              </li>
+              <li class="specialist__item swiper-slide" id="sherbiy">
+
+
+
+                <div class="specialist__item-img">
+
+                  <img src="assets/img/expert1-8.webp" alt="Вікторія Щербій" loading="lazy">
+
+                </div>
+
+
+
+                <div class="specialist__item-info">
+
+                  <div class="specialist__item-inner">
+
+                    <div class="grid-spec">
+
+                      <span class="specialist__item-spec">психолог</span>
+
+                      <h3 class="specialist__item-title">Уляна Давид</h3>
+
+                    </div>
+
+                    <ul class="specialist__item-list">
+
+                      <li class="specialist__item-merit">
+
+                        <img src="assets/img/specialist__item-img1.svg" alt="1">
+
+                        <span class="specialist__item-merit-txt">Практико-орієнтовне навчання у МВС</span>
+
+                      </li>
+
+                      <li class="specialist__item-merit">
+
+                        <img src="assets/img/specialist__item-img2.svg" alt="2">
+
+                        <span class="specialist__item-merit-txt">Активна практика з дітьми різного віку</span>
+
+                      </li>
+
+                      <li class="specialist__item-merit">
+
+                        <img src="assets/img/specialist__item-img3.svg" alt="3">
+
+                        <span class="specialist__item-merit-txt">Робота у напрямку КПТ</span>
+
+                      </li>
+
+                    </ul>
+
+
+                  </div>
+
+                  <button class="specialist__item-button">Записатися</button>
+
+                </div>
+
+
+
+            </ul>
+            <div class="spec-pagination"></div>
+          </div>
+        </div>
+      </section>
+
+  <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+  <script src="https://unpkg.com/imask"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+  <script type="text/javascript" src="assets/js/main.js"></script>
+
+      <script >
+    function updatePage() {
+
+      let curWidth = $(window).width();
+
+      $(window).resize(function () {
+        if ($(window).width() <= 991 && curWidth > 991) {
+
+          location.reload();
+        }
+        else if ($(window).width() > 991 && curWidth <= 991) {
+
+          location.reload();
+        }
+      });
+    }
+    if ($('#home').length === 1) {
+      updatePage();
+    }
+
+    function sliderSpec() {
+      var swiper1 = new Swiper(".sliderSpec", {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        preloadImages: false,
+      });
+    }
+    if ($('#home').length === 1) {
+      if ($(window).width() < 991) {
+        sliderSpec();
+      }
+    }
+    function sliderPrice() {
+      var swiper2 = new Swiper(".sliderPrice", {
+        slidesPerView: 3,
+        spaceBetween: 60,
+        pagination: {
+          el: '.price-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          /* when window width is >= 320px */
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          /* when window width is >= 480px */
+          700: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          /* when window width is >= 640px */
+          1279: {
+            slidesPerView: 3,
+            spaceBetween: 20
+          }
+        }
+      });
+    }
+    function sliderSpec() {
+      var swiper2 = new Swiper(".sliderSpec", {
+        slidesPerView: 3,
+        spaceBetween: 60,
+        pagination: {
+          el: '.spec-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          /* when window width is >= 320px */
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          /* when window width is >= 480px */
+          700: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+          },
+          /* when window width is >= 640px */
+          1279: {
+            slidesPerView: 3.5,
+            spaceBetween: 20
+          }
+        }
+      });
+    }
+    if ($('#home').length === 1) {
+      sliderPrice();
+      sliderSpec();
+    }
+
+    function sliderRev() {
+      var swiper3 = new Swiper(".sliderRev", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        preloadImages: false,
+        pagination: {
+          el: '.sec-rev-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          /* when window width is >= 320px */
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+          },
+          /* when window width is >= 480px */
+          700: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          /* when window width is >= 640px */
+          1279: {
+            slidesPerView: 3,
+            spaceBetween: 100
+          }
+        }
+      });
+    }
+    if ($('#home').length === 1) {
+      sliderRev();
+    }
+
+    let elements = document.getElementsByClassName('form-tel');
+    for (let i = 0; i < elements.length; i++) {
+      new IMask(elements[i], {
+        mask: '+{38}(000)000-0000',
+      });
+    }
+
+    Fancybox.bind('[data-fancybox="gallery"]', {
+      Thumbs: {
+        autoStart: false,
+      },
+      Toolbar: {
+        display: [
+          "zoom",
+          "slideshow",
+          "fullscreen",
+          "close",
+        ],
+      },
+    });
+  </script>
