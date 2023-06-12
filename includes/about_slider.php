907fd9bce0ebecb7b8309911c59fbea3aaf63f5e
Alexander Kostin <asurv2@gmail.com>
2023-06-12 17:37:39 +0300

    update

--- includes/about_slider.php
+++ includes/about_slider.php
@@ -6,6 +6,7 @@
     <div class="sliderRev" data-aos="fade-down">
       <div class="swiper-container">
         <div class="swiper-wrapper">
+
           <div class="swiper-slide">
             <div class="sec-rev__item" data-fancybox="gallery" data-src="assets/img/review1.jpg">
               <img src="assets/img/review1.jpg" width="370" alt="#" loading="lazy">
@@ -31,6 +32,7 @@
               <img src="assets/img/review5.jpg" width="370" alt="#" loading="lazy">
             </div>
           </div>
+          
         </div>
         <div class="sec-rev-pagination"></div>
       </div>
