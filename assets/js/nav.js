0d2b75379a2c229befa3aa75bd874bb8b15f71d4
Alexander Kostin <asurv2@gmail.com>
2023-06-15 00:02:08 +0300

    update code for nav menu

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,17 +1,33 @@
-// Получаем текущий URL
-var currentUrl = window.location.href;
+// var currentUrl = window.location.href;
 
-// Получаем все ссылки в меню
-var menuLinks = document.querySelectorAll('.nav__list-item-link');
+// // Получаем все ссылки в меню
+// var menuLinks = document.querySelectorAll('.nav__list-item-link');
 
-// Перебираем ссылки и проверяем совпадение с текущим URL
-menuLinks.forEach(function(link) {
-  var linkUrl = link.href;
+// // Перебираем ссылки и проверяем совпадение с текущим URL
+// menuLinks.forEach(function(link) {
+//   var linkUrl = link.href;
 
-  // Если текущий URL содержит URL ссылки, добавляем класс is-active к родительскому элементу li
-  if (currentUrl === linkUrl) {
-    link.parentNode.classList.add('is-active');
-  } else {
-    link.parentNode.classList.remove('is-active');
-  }
+//   // Если текущий URL содержит URL ссылки, добавляем класс is-active к родительскому элементу li
+//   if (currentUrl === linkUrl) {
+//     link.parentNode.classList.add('is-active');
+//   } else {
+//     link.parentNode.classList.remove('is-active');
+//   }
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var currentUrl = window.location.href;
+  var menuLinks = document.querySelectorAll('.nav__list-item-link');
+
+  menuLinks.forEach(function(link) {
+    var linkUrl = link.href;
+
+    if (currentUrl === linkUrl) {
+      link.parentNode.classList.add('is-active');
+    } else {
+      link.parentNode.classList.remove('is-active');
+    }
+  });
 });
